029760dc87f7c3020393f1f5b3ccab99d59b8f2f
curry9999 <curry123456789@gmail.com>
2021-02-18 00:25:18 +0900

    Add vpc

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
 import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
+import ec2 = require('@aws-cdk/aws-ec2');
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    // VPC
+    new ec2.Vpc(this, 'VPC', {
+      cidr: "10.0.0.0/16",
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'ingress',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'application',
+          subnetType: ec2.SubnetType.PRIVATE,
+        },
+        {
+          cidrMask: 28,
+          name: 'rds',
+          subnetType: ec2.SubnetType.ISOLATED,
+        }
+      ]
+    });
   }
 }
 
