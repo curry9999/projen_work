96ca8c3b8eab6113603eda537320f79d22f0a670
curry9999 <curry123456789@gmail.com>
2021-02-20 08:26:55 +0900

    add lambda and servie role

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,53 @@
 import { BackupPlan, BackupPlanRule, BackupResource } from '@aws-cdk/aws-backup';
 import { AttributeType, Table } from '@aws-cdk/aws-dynamodb';
+import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
+import { Role, ServicePrincipal, ManagedPolicy } from '@aws-cdk/aws-iam';
 import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
 
+// DynamoDB Stack
 export class DynamoDBStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
+    // Lambda Service Role
+    const servicerole = new Role(this, 'IamRoleLambda', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+        ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'),
+      ],
+    }); 
+
     // Create DynamoDB Table
     const table = new Table(this, 'DynamodbTable', {
-      tableName: 'picturetable',
       partitionKey: {
         name: 'no',
-        type: AttributeType.NUMBER,
+        type: AttributeType.STRING,
       },
+      sortKey: {
+        name: 'col',
+        type: AttributeType.STRING,
+      }
     });
 
     // Create AWS Backup
-    const backupplan = new BackupPlan(this, 'BackupPlan', {
-      backupPlanName: 'backup-plan',
-    });
+    const backupplan = new BackupPlan(this, 'BackupPlan', {});
     backupplan.addRule(BackupPlanRule.daily());
     backupplan.addSelection('Selection', {
       resources: [
         BackupResource.fromDynamoDbTable(table), // A DynamoDB table
       ],
+    });
+
+    // Lambda
+    new Function(this, 'HelloWorld', {
+      handler: 'index.handler',
+      code: Code.fromAsset('functions'),
+      runtime: Runtime.NODEJS_14_X,
+      environment: {
+        tablename: table.tableName,
+      },
+      role: servicerole,
     });
   }
 }
