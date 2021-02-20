4c45b8a4259148a86d887a9c52476272350558e6
curry9999 <curry123456789@gmail.com>
2021-02-21 00:32:21 +0900

    cdk version v1.90.1

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.90.0',
+  cdkVersion: '1.90.1',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
   name: 'projen_work',
